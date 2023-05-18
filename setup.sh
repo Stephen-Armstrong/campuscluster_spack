e1c5125c3ba845a3de709251c155a29fd7aacbda
logantm2 <loganmeredit@gmail.com>
2023-05-18 14:08:25 -0500

    Update update files

--- setup.sh
+++ setup.sh
@@ -9,4 +9,7 @@ echo "- $PWD/lcpp-spack-repo" >> ../spack/etc/spack/repos.yaml
 # Remove user-level compiler list. They are almost definitely broken.
 rm ~/.spack/linux/compilers.yaml
 sh install_hpic2deps.sh
+spack module tcl refresh --delete-tree -y
+. ../spack/share/spack/setup-env.sh
+module refresh
 
