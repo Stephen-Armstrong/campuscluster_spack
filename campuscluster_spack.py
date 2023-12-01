0c1bf7eff0269ee0723e42a19f987d1355c3a67a
Logan Meredith <loganmeredit@gmail.com>
2023-12-01 08:54:00 -0600

    Don't build hdf5 tests

--- campuscluster_spack.py
+++ campuscluster_spack.py
@@ -179,7 +179,7 @@ cd ../..
 mkdir hdf5_dev && cd hdf5_dev
 git clone git@github.com:HDFGroup/hdf5.git
 mkdir build && cd build
-cmake ../hdf5 -DCMAKE_BUILD_TYPE={build_type} -DHDF5_BUILD_EXAMPLES=OFF -DHDF5_ENABLE_PARALLEL=ON -DHDF5_BUILD_CPP_LIB=ON -DALLOW_UNSUPPORTED=ON -DCMAKE_INSTALL_PREFIX=../install
+cmake ../hdf5 -DCMAKE_BUILD_TYPE={build_type} -DHDF5_BUILD_EXAMPLES=OFF -DHDF5_ENABLE_PARALLEL=ON -DHDF5_BUILD_CPP_LIB=ON -DALLOW_UNSUPPORTED=ON -DCMAKE_INSTALL_PREFIX=../install -DBUILD_TESTING=OFF
 make -j{num_build_cores}
 make install
 cd ../..
