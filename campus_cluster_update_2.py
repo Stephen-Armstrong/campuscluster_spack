f69e079e2937e8bf5867dba92adf1ad089a63445
Stephen Armstrong <sda3@illinois.edu>
2024-09-26 13:22:32 -0500

    Update campus_cluster_update_2.py

--- campus_cluster_update_2.py
+++ campus_cluster_update_2.py
@@ -33,7 +33,7 @@ cuda_arch_options = [70]#, 80, 86, 90, None]
 #     Stephen Armstrong
 #     Andrew Liu
 #     Logan Meredith
-build_types_arr = ["Debug", "Release"]
+build_types_arr = ["Release", ] #"Debug",
 
 def update():
     cmake_module = f"{top_level_dir}/modulefiles/cmake"
@@ -178,11 +178,13 @@ cd {top_level_dir}/builds/{dir_name}
             kokkos_cmake_cmd += f" -DKokkos_ENABLE_OPENMP={'ON' if openmp_option else 'OFF'}"
             
             """
-            hypre_configure_cmd = f"./configure"
+            hypre_configure_cmd = f"./configure --enable-shared=YES --prefix={build_dependent_dir_path}/hypre_dev/install"
+            if build_type == "Debug":
+                hypre_configure_cmd += f" --enable-debug=YES"
             if openmp_option:
-                hypre_configure_cmd += f" --with-openmp"
+                hypre_configure_cmd += f" --with-openmp=YES"
             if cuda_enabled:
-                hypre_configure_cmd += f" --with-kokkos --with-cuda --with-gpu-arch={cuda_arch_option}"
+                hypre_configure_cmd += f" --with-kokkos=YES --with-cuda=YES --with-gpu-arch={cuda_arch_option}"
                 '''
                 if cuda_arch_option == 70:
                     hypre_configure_cmd += f" --with-gpu-arch=70"
