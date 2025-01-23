c8da315d02c6859f356217db8451ad3e2a052560
Stephen Armstrong <sda3@illinois.edu>
2025-01-23 10:06:37 -0600

    Update campus_cluster_update_2.py

--- campus_cluster_update_2.py
+++ campus_cluster_update_2.py
@@ -26,7 +26,7 @@ python_module = "anaconda/3"
 
 #New modules
 #Intel compiler may not be needed, but in the event that cmake doesn't work, the intel compiler can generate an basic +omp-cuda build.
-compiler_module = "gcc/13.3.0 intel/tbb intel/compiler-rt intel/umf intel/compiler/2025.0.4"
+compiler_module = "gcc/13.3.0"# intel/tbb intel/compiler-rt intel/umf intel/compiler/2025.0.4"
 mpi_module = "openmpi/5.0.1-gcc-13.3.0"
 cuda_module = "cuda/12.6" #11.6
 python_module = "python/3.10.16"
