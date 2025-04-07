22e88b5e539f87f230f049578b526c919cae80eb
Stephen Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-04-07 13:32:15 -0500

    Update campus_cluster_update_2.py

--- campus_cluster_update_2.py
+++ campus_cluster_update_2.py
@@ -37,7 +37,7 @@ num_build_cores = len(os.sched_getaffinity(0)) #4
 num_versions_kept = 3
 #Module Compile options for OpenMP and CUDA
 openmp_options = [True]#, False]
-cuda_arch_options = [None, 70, 80, 86, 90]
+cuda_arch_options = [None]#, 70, 80, 86, 90]
 
 #Don't edit the following lines for normal operations
 #WHO SHOULD BE EDITING THE FOLLOWING LINES:
