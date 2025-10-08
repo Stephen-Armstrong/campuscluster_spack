776bedec3731321406e13835cb75ac51b0cd489b
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-08 17:39:57 -0500

    change file name saved as

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 module load python/3.13.2
-(sh -c "python3 campuscluster_update.py update" &> output_update.log) & echo $! > update.pid # Or whichever one is actually working now
+(sh -c "python3 campuscluster_update.py update" &> output_update.log) & echo $! > output_update.pid # Or whichever one is actually working now
