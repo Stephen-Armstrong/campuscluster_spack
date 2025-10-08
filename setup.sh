3d250bc42d3763559dfceeac1f3cc7049ca3adcd
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-08 13:12:48 -0500

    more fixing spack repo stuff.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git clone -c feature.manyFiles=true https://github.com/spack/spack.git
 cd -
 cp spack_config/* ../spack/etc/spack/.
 echo "repos:" > ../spack/etc/spack/repos.yaml
-echo "- $PWD/lcpp-spack-repo" >> ../spack/etc/spack/repos.yaml
+echo "- $PWD/spack_repo/lcpp-spack-repo" >> ../spack/etc/spack/repos.yaml
 # Remove user-level compiler list. They are almost definitely broken.
 rm ~/.spack/linux/compilers.yaml
 sh install_hpic2deps.sh
