e4d7ac86a41125e74d0b0ed77817d4b1a44459cc
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-08 17:21:02 -0500

    sub shell so non blocking?

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 module load python/3.13.2
-python3 campus_cluster_update_2.py update &> output_update.log # Or whichever one is actually working now
+sh -c "python3 campus_cluster_update_2.py update" &> output_update.log # Or whichever one is actually working now
