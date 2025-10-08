981385b6b36b4038a50fbf4a7fdd47cdc0556072
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-08 17:15:24 -0500

    missign a command line var

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 module load python/3.13.2
-python3 campus_cluster_update_2.py > update_output.log # Or whichever one is actually working now
+python3 campus_cluster_update_2.py update > update_output.log # Or whichever one is actually working now
