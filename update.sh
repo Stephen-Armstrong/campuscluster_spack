e4a402b0b1e22dd768046f461ad7d50e901df69c
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-09 10:39:32 -0500

    Trying to use spack python to maybe fix not finding the right gcc version.

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 module load python/3.13.2
-(sh -c "python3 campuscluster_update.py update" &> output_update.log) & echo $! > output_update.pid # Or whichever one is actually working now
+(sh -c "spack python campuscluster_update.py update" &> output_update_spack_py.log) & echo $! > output_update_spack_py.pid # Or whichever one is actually working now
